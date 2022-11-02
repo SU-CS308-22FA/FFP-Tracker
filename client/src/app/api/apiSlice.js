01f3e1d6207a4719939385e328f610a7e914ef5e
Efe Tuzun <efetuzun@sabanciuniv.edu>
2022-11-02 10:03:43 +0300

    Api Slice edited

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://ffp-tracker.herokuapp.com",
+  }),
   endpoints: (builder) => ({}),
 });
