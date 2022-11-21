2c8aea4c5beb7210d21666e8abeff85fbe4534fb
eggmnn <71037775+eegmnn@users.noreply.github.com>
2022-11-21 23:14:07 +0300

    Delete and create buttons are working on notifications listed on user page

--- client/src/components/ProfilePageComponent.js
+++ client/src/components/ProfilePageComponent.js
@@ -7,87 +7,121 @@ import { Typography } from "@mui/material";
 import { useContext } from "react";
 import { UserContext } from "../contexts/userContext";
 import FFP_API from "../app/api";
+import { Box } from "@mui/system";
+import { useState, useEffect } from "react";
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+
 
 
 
 export default function ProfilePageComponent() {
+  const[notifications, setNotifications] = useState(null);
+  const[user, setUser] = useState(null);
+
   const { id } = useParams();
-  const { user, setUser } = useContext(UserContext);
+  useEffect(() => {
+    
+    const fetchNotifications = async () => {
+      const response = await FFP_API.get(`/notifications/`);
+      console.log(response.data);
+      setNotifications(response.data);
+    };
+    const fetchUser = async () => {
+      const response = await FFP_API.get(`/users/${id}`);
+      setUser(response.data);
+    };
+    fetchNotifications();
+    fetchUser();
+    
+  }, [setUser, setNotifications, id]);
 
-  const navigate = useNavigate();
-  const unreadNotifications = user.notifications.filter(
-    (notification) => notification.read === false
-  );
-  const readNotifications = user.notifications.filter(
-    (notification) => notification.read === true
-  );
 
-// create a notification template showing the notification subject, message, from and read status
-  const notificationTemplate = (notification) => {
-    return (
-      <div>
-        <Typography variant="body2" color="text.secondary">
-          From: {notification.from}
-        </Typography>
-        <Typography variant="h6" component="h2">
-          Subject: {notification.subject}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" gutterBottom>
-          Message: "{notification.message}"
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {notification.read ? "Read" : "Unread"}
-        </Typography>
-      </div>
-    );
+  // get user name from user id
+  const getUserName = (senderid) => {
+    const user = FFP_API.get(`/users/${senderid}`);
+    return user.data.fullname;
+  }; 
+
+// mark a notification as read
+  const markAsRead = async (notificationid) => {
+    const response = await FFP_API.patch(`/notifications/${notificationid}`);
+    console.log(response.data);
+    setNotifications(response.data);
+  };
+
+  // delete a notification
+  const deleteNotification = async (notificationid) => {
+    const response = await FFP_API.delete(`/notifications/${notificationid}`);
+    console.log(response.data);
+    setNotifications(response.data);
+  };
+
+// create a notification
+  const createNotification = async (senderid, recieverid, subject, message) => {
+    const response = await FFP_API.post(`/notifications/`, {
+      sender: senderid,
+      reciever: recieverid,
+      subject: subject,
+      message: message,
+    });
+    console.log(response.data);
+    setNotifications(response.data);
   };
 
 
 
 
+// create a notification template showing the notification subject, message, from and read status using list and list items
+  const notificationTemplate = (notification) => {
+    return (
+      <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+        <ListItem>
+          <ListItemText primary={notification.subject} secondary={notification.message} />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem>
+          <ListItemText primary={notification.sender}/>
+          <Button variant="contained" onClick={() => markAsRead(notification._id)}>Mark as Read</Button>
+          <Button variant="contained" onClick={() => deleteNotification(notification._id)}>Delete</Button>
+        </ListItem>
+        
+      </List>
+    );
+  };
+
+
 // represent unread and read notifications in two separate lists
-  const allNotifications = (
-    <Container>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Notifications
-      </Typography>
-      <Typography variant="h5" component="h2" gutterBottom>
-        Unread
-      </Typography>
-      {unreadNotifications.length > 0 ? (
-        unreadNotifications.map((notification) => (
-          <Container key={notification._id}>
-          <div key={notification._id}>
-          {notificationTemplate(notification)}  
-          </div>
-          </Container>
-        ))
-      ) : (
-        <CircularProgressComponent />
-      )}
-      <Typography variant="h5" component="h2" gutterBottom>
-        Read
-      </Typography>
-      {readNotifications.length > 0 ? (
-        readNotifications.map((notification) => (
-          <div key={notification._id}>
-            <Typography variant="body1" gutterBottom>
-              {notification.message}
-            </Typography>
-          </div>
-        ))
-      ) : (
-        <CircularProgressComponent />
-      )}
-    </Container>
-  );
+  function showNotifications(notifications) {
+    let unread = [];
+    let read = [];
+    for (const [key, value] of Object.entries(notifications)) {
+      if (value.read === false && value.reciever === user._id) {
+        unread.push(value);
+      } else if (value.read === true && value.reciever === user._id) {
+        read.push(value);
+      }
+    }
+    return (
+      <div>
+        <Typography variant="h5" component="h2" gutterBottom>
+          Unread Notifications
+        </Typography>
+        {unread.map((notification) => ( notificationTemplate(notification)))}
+        <Typography variant="h5" component="h2" gutterBottom>
+          Read Notifications
+        </Typography>
+        {read.map((notification) => ( notificationTemplate(notification)))}
+      </div>
+    );
+  }
 
-
-
-
+  
 
   const content = !user ? (
-    <Navigate to="/login" />
+    <CircularProgressComponent />
   ) : (
     <>
       <Container component="main" maxWidth="md">
@@ -96,10 +130,11 @@ export default function ProfilePageComponent() {
         </Typography>
         <Typography variant="body1" sx={{ m: 2 }} align="center">
           As a {user.role}, you can edit your profile, submit and review files,
-          and much more thingss!
+          and much more things!
         </Typography>
-        {allNotifications}
-        
+        <Button variant="contained" sx={{ m: 2 }} onClick={() => createNotification("636f949c4f57d04b87cb2b98", "636abddd67312dcd2ad11076", "Test Subject", "Test Message")}> Create Notification</Button>
+        {showNotifications(notifications)}
+              
       </Container>
     </>
   );
--- server/controllers/notificationsController.js
+++ server/controllers/notificationsController.js
@@ -2,6 +2,7 @@ const Notification = require("../models/Notification");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
 
+
 // @desc Get all notifications
 // @route GET /notifications
 // @access public
@@ -10,242 +11,96 @@ const getAllNotifications = asyncHandler(async (req, res) => {
     res.json(notifications);
     });
 
-const getAllReadNotifications = asyncHandler(async (req, res) => {
-    const notifications = await Notification.find({ read: true }).lean();
-    res.json(notifications);
-    });
-
-const getAllUnreadNotifications = asyncHandler(async (req, res) => {
-    const notifications = await Notification.find({ read: false }).lean();
-    res.json(notifications);
-    });
-
-// @desc Get a notification by id
-// @route GET /notifications/:id
-// @access private
 const getNotificationById = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const notification = await Notification.findOne({ _id: id
-    }).select("-__v");
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
+    const { id } = req.params.id;
+    const notification = await Notification.findOne({ _id: id }).lean();
+    if (!notification) 
+        return res.status(404).json({ error: "Notification not found!" });
     return res.status(200).json(notification);
-    });
+});
 
-// @desc Create a new notification
-// @route POST /notifications
-// @access private
+
+// create a new notification with a sender and reciever
 const createNotification = asyncHandler(async (req, res) => {
-    const { userId, from, message, subject } = req.body;
-    if (!userId || !from || !message || !subject) {
+    const { reciever, sender, message, subject } = req.body;
+    if (!reciever || !sender || !message || !subject) {
         return res.status(400).json({ error: "All fields are required!" });
-        }
-    const user = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).json({ error: "User not found!" });
+    }
     const notification = await Notification.create({
-        userId,
-        from,
-        message,
-        subject,
-        });
-    return res.status(201).json(notification);
+        reciever: reciever,
+        sender: sender,
+        message: message,
+        subject: subject,
+        read: false,
     });
+    return res.status(201).json(notification);
+});
 
-// @desc Update a notification by id
-// @route PATCH /notifications/:id
-// @access private
+
+
+// update a notification by notification id
 const updateNotificationById = asyncHandler(async (req, res) => {
     const { id } = req.params;
-    const { read } = req.body;
-    const {from, message, subject } = req.body;
-
-    const notification = await Notification
-        .findOne
-        ({
-            _id: id
-        });
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    notification.read = read;
-    notification.from = from;
-    notification.message = message;
-    notification.subject = subject;
+    const { read, sender, reciever, subject, message} = req.body;
+    if (!read || !sender || !reciever || !subject || !message) {
+        return res.status(400).json({ error: "All fields are required to update a notification!" });
+    }
+    const notification = await Notification.findOne({ _id: id });
+    if (!notification) 
+        return res.status(404).json({ error: "Notification not found!" });
+    notification.read.set(read);
+    notification.sender.set(sender);
+    notification.reciever.set(reciever);
+    notification.subject.set(subject);
+    notification.message.set(message);
     const result = await notification.save();
     return res.status(200).json(notification);
-    });
+});
 
-// @desc Delete a notification by id
+// @desc make a notification read
+// @route PATCH /notifications/:id
+// @access public
+const markAsRead = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+    const notification = await Notification
+        .findOne ({ _id: id })
+        .lean();
+    if (!notification)
+        return res.status(404).json({ error: "Notification not found!" });
+    notification.read.set(true);
+    notification.read = true;
+
+    const result = await notification.save();
+    return res.status(200).json(notification);
+});
+
+
+// @desc delete a notification by id
 // @route DELETE /notifications/:id
-// @access private
+// @access public
 const deleteNotificationById = asyncHandler(async (req, res) => {
     const { id } = req.params;
-    const notification = await Notification.findOne({ _id: id });
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    await notification.remove();
-    return res.status(200).json({ message: "Notification deleted!" });
-    });
-
-// change a notification to read
-const markNotificationAsRead = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const notification = await Notification.findOne({ _id: id });
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    notification.read = true;
-    await notification.save();
-    return res.status(200).json({ message: "Notification marked as read!" });
-    });
-
-// change a notification to unread
-const markNotificationAsUnread = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const notification = await Notification.findOne({ _id: id });
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    notification.read = false;
-    await notification.save();
-    return res.status(200).json({ message: "Notification marked as unread!" });
-    });
-
-// post a new notification users notification list
-const postNotification = asyncHandler(async (req, res) => {
-    const { userId, from, message, subject } = req.body;
-    if (!userId || !from || !message || !subject) {
-        return res.status(400).json({ error: "All fields are required!" });
-        }
-    const user
-        = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    const notification = await Notification.create({ userId, from, message, subject });
-    user.notifications.push(notification);
-    await user.save();
-    return res.status(201).json(notification);
-    });
-
-
-// delete a notification from users notification list
-const deleteNotificationFromUser = asyncHandler(async (req, res) => {
-    const { userId, notificationId } = req.body;
-    if (!userId || !notificationId) {
-        return res.status(400).json({ error: "All fields are required!" });
-        }
-    const user = await User
-        .findOne
-        ({
-            _id: userId
-        });
-    if (!user) return res.status(404).json({ error: "User not found!" });
     const notification = await Notification
         .findOne
         ({
-            _id: notificationId
-        });
+            _id: id
+        })
+        .lean();
     if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    user.notifications.pull(notification);
-    await user.save();
+        return res.status(404).json({ error: "Notification not found!" });
+    const result = await Notification.deleteOne({ _id: id });
     return res.status(200).json({ message: "Notification deleted!" });
-    });
-
-// @desc Get all notifications for a user
-// @route GET /notifications/user/:id
-// @access private
-const getAllNotificationsForUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const user
-        = await User.findOne({ _id: id }).populate("notifications");
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    return res.status(200).json(user.notifications);
-    });
-
-// @desc Get all read notifications for a user
-// @route GET /notifications/user/:id/read
-// @access private
-const getAllReadNotificationsForUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const user = await User
-        .findOne
-        ({
-            _id: id
-        }).populate("notifications");
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    const readNotifications = user.notifications.filter(
-        (notification) => notification.read === true
-        );
-    return res.status(200).json(readNotifications);
-    });
-
-// @desc Get all unread notifications for a user
-// @route GET /notifications/user/:id/unread
-// @access private
-const getAllUnreadNotificationsForUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const user = await User
-        .findOne
-        ({
-            _id: id
-        }).populate("notifications");
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    const unreadNotifications = user.notifications.filter(
-        (notification) => notification.read === false
-        );
-    return res.status(200).json(unreadNotifications);
-    });
-
-// @desc Get a notification by id for a user
-// @route GET /notifications/user/:id/:notificationId
-// @access private
-const getNotificationByIdForUser = asyncHandler(async (req, res) => {
-    const { id, notificationId } = req.params;
-    const user = await User
-        .findOne
-        ({
-            _id: id 
-        }).populate("notifications");
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    const notification = user.notifications.find(
-        (notification) => notification._id == notificationId
-        );
-    if (!notification)
-        return res.status(404).json({ error: "No notification found!" });
-    return res.status(200).json(notification);
-    });
-
-// @desc Send a notification to a user from a user
-// @route POST /notifications/user/:id
-// @access private
-const sendNotificationToUser = asyncHandler(async (req, res) => {
-    const { id } = req.params;
-    const { from, message, subject } = req.body;
-    if (!from || !message || !subject) {
-        return res.status(400).json({ error: "All fields are required!" });
-        }
-    const user = await User
-        .findOne
-        ({
-            _id: id
-        });
-    if (!user) return res.status(404).json({ error: "User not found!" });
-    const notification = await Notification.create({ userId: id, from, message, subject });
-    user.notifications.push(notification);
-    await user.save();
-    return res.status(201).json(notification);
-    });
-
-
-
+});    
 
 
 module.exports = {
     getAllNotifications,
-    getAllReadNotifications,
-    getAllUnreadNotifications,
     getNotificationById,
     createNotification,
     updateNotificationById,
+    markAsRead,
     deleteNotificationById,
-    markNotificationAsRead,
-    markNotificationAsUnread,
-    };
+};
+
+
 
