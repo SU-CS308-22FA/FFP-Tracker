4cdfe40f14543cbdaa1e23b3845ae0508b482e8f
Efe Tuzun <efetuzun@sabanciuniv.edu>
2022-12-07 15:21:06 +0300

    File Submission Function Added
    Add the createSubmission method to backend to create a new submission
    in the database. Add the request that sends the link, date, team
    information and file information to the backend. Check in the front end
    if the file is selected. If not, raise an error. Add the routers to
    the corresponding route file.

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -31,8 +31,32 @@ const getFilesByTeamId = asyncHandler(async (req, res) => {
   return res.status(200).json(files);
 });
 
+// @desc Create a file submission
+// @route POST /files/team/:id
+// @access private
+const createSubmission = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { file, submitDate } = req.body;
+  if (!file || !submitDate)
+    return res.status(400).json({ error: "Missing required fields" });
+  const team = await Team.findOne({ _id: id }).lean();
+  if (!team) return res.status(404).json({ error: "Team not found!" });
+  const newFile = await File.create({
+    name: `${team.teamName} - ${submitDate}`,
+    url: file,
+    teamName: team.teamName,
+    submitDate,
+    teamId: id,
+    currentStatus: "Submitted",
+    lastUpdated: submitDate,
+  });
+  if (!newFile) return res.status(400).json({ error: "Error creating file" });
+  return res.status(200).json(newFile);
+});
+
 module.exports = {
   getAllFiles,
   getFileById,
   getFilesByTeamId,
+  createSubmission,
 };
